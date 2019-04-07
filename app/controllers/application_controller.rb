8f0cb0828cb3bb0bd465c307c8d891b0f75e17de
bitcraze <bitcraze@bitcraze-vm.unassigned-domain>
2019-04-07 16:48:02 +0200

    exclude devise_controller from user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	include ApplicationConcern
 
   	protect_from_forgery with: :exception
-  	before_action :authenticate_user!
+  	before_action :authenticate_user!, unless: [:devise_controller?]
 	before_action :set_organization, unless: [:organization_controller?, :devise_controller?]
 
   	def set_organization
